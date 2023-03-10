2cf3ddbbd43bd617081f93c04156b44ddf216bb8
Raurut <makey@yandex.ru>
2023-03-10 17:10:41 +0300

    Task 66 complete

---
+++ Task 66/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+Clear();
+int m = InputInt("Введите M: ");
+int n = InputInt("Введите N: ");
+WriteLine($"Сумма элементов от {m} до {n} = {CountNaturalSum(m, n)}");
+
+int InputInt(string output)
+{
+    Write(output);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int CountNaturalSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + CountNaturalSum(m, n - 1);
+}
