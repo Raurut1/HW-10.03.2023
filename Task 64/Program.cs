8f3fa709a582adcbb28e063f655a3ac091f7a349
Raurut <makey@yandex.ru>
2023-03-10 17:07:52 +0300

    Task 64 complete

---
+++ Task 64/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+Clear();
+int n = InputInt("Введите положительное число: ");
+int m = 1;
+WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
+
+int InputInt(string output)
+{
+    Write(output);
+    return int.Parse(ReadLine()!);
+}
